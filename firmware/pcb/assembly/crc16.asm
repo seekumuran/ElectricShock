33e2b3808a1fa72e64c6a38b8661510f1b3df1eb
kumaran <k31311558@gmail.com>
2026-09-05 23:08:22 +0530

    Create crc16.asm

---
+++ firmware/pcb/assembly/crc16.asm
@@ -0,0 +1,26 @@
+; ============================================================
+; ELECTRIC SHOCK
+; PCB CRC16
+; ============================================================
+
+CRC16_UPDATE:
+
+            XOR CRC_H, ACC
+
+            MOV BIT_COUNT, #8
+
+CRC_LOOP:
+
+            TEST CRC_H, #0x80
+
+            SHL CRC_L
+            ROL CRC_H
+
+            IF_SET:
+                XOR CRC_H, #0x10
+                XOR CRC_L, #0x21
+
+            DEC BIT_COUNT
+            JNZ CRC_LOOP
+
+            RET
