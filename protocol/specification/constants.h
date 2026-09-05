c3b6379b02e1b92dd7e579383ae1e6aef429d27b
kumaran <k31311558@gmail.com>
2026-09-05 23:05:36 +0530

    Add constants for electric shock protocol

---
+++ protocol/specification/constants.h
@@ -0,0 +1,13 @@
+#ifndef ELECTRIC_SHOCK_PROTOCOL_H
+#define ELECTRIC_SHOCK_PROTOCOL_H
+
+#define ES_SYNC_BYTE        0xA5
+
+#define ES_MSG_INPUT        0x01
+#define ES_MSG_STATUS       0x02
+#define ES_MSG_ACK          0x06
+#define ES_MSG_ERROR        0x15
+
+#define ES_MAX_PAYLOAD      64
+
+#endif
